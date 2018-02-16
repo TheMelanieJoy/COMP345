087f1c17832cfc3611679ece604193a6f66b5ae1
TheMelanieJoy <m.damilig@hotmail.com>
2018-02-16 01:05:17 -0500

    Update dice

--- src/Dice.h
+++ src/Dice.h
@@ -1,11 +1,4 @@
 #pragma once
 #define DICE_H
 
-#include <ctime>
-#include <vector>
-#include <iostream>
-using namespace std;
-
-std::vector<int> roll(int);
-
-int diceSum(std::vector<int>);
+int roll(int);
--- src/SmallWorld.cpp
+++ src/SmallWorld.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
+#include "dice.h"
+#include <iostream>
+using namespace std;
 
+
+private void setup() {
+    
+}
 
 int main()
 {
+    cout << "Part 3: Reinforcements Dice" << std::endl;
+    cout << "Rolling die 1..." << std::endl;
+    int dice1 = Dice::roll();
+    
     return 0;
 }
 
