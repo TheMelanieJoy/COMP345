790ac8e2d6f688c6472cb5eaa87ced5211fed5c1
MarkChilev <36088102+MarkChilev@users.noreply.github.com>
2018-04-12 23:05:45 -0400

    CoMmEnTs

--- src/MapReader.h
+++ src/MapReader.h
@@ -4,10 +4,14 @@
 #include "Map.h"
 #include <string>
 
+//used to convert a .map file into a map we can use
 class MapReader {
 private:
+	//where the map is located on the pc
 	std::string path = "";
 public:
+	//Constructor, sets the path
 	MapReader(std::string p) : path(p) {};
+	//Parses the .map file and returns an usable map
 	Map makeMap();
 };
